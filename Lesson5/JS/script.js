3c674648e3b6879e1c021b2849c9a3023a660c5a
piotr.s.herman@gmail.com <piotr.s.herman@gmail.com>
2021-10-14 16:39:22 +0300

    commit

--- Lesson5/JS/script.js
+++ Lesson5/JS/script.js
@@ -3,7 +3,49 @@ var i;
 
 for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("/u00D7");
+    var txt = document.createTextNode("\u00D7");
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+function newElement() {
+    var li = document.createElement('li');
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.creatrTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("It should be entered");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
