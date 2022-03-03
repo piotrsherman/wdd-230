f3ce28c9c3e8feedc40fe16aea055c41cddfb601
piotr.s.herman@gmail.com <piotr.s.herman@gmail.com>
2022-03-04 00:02:44 +0300

    commit

---
+++ lesson9/prophets-files/prophets.js
@@ -0,0 +1,24 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const prophets = jsonObject['prophets']
+        console.table(jsonObject);
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement("section");
+            let h2 = document.createElement("h2");
+            let birthDate = document.createElement("p");
+            let birthPlace = document.createElement("p");
+            let image = document.createElement("img");
+            card.setAttribute("class", `order${prophets[i].order}`)
+            h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
+            birthDate.textContent =`Birth Date: ${prophets[i].birthdate}`;
+            birthPlace.textContent =`Birth Place: ${prophets[i].birthplace}`;
+            image.setAttribute("src", prophets[i].imageurl);
+            image.setAttribute( "alt", `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`);
+            card.appendChild(h2); card.appendChild(birthDate); card.appendChild(birthPlace); card.appendChild(image);
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
