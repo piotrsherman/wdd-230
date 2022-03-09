9ffbea44958cb5bf6ec237aa7c2e215dceb4357d
piotr.s.herman@gmail.com <piotr.s.herman@gmail.com>
2022-03-09 20:34:56 +0300

    commit

--- chamber/Directory_files/directory.js
+++ chamber/Directory_files/directory.js
@@ -1,95 +1,50 @@
-// const requestURL = 'https://anesushangwa.github.io/wdd230/Lesson9/datajson/data.json';
-// const business = document.querySelector('.business');
-
-// fetch(requestURL)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (jsonObject) {
-//     console.table(jsonObject);  // temporary checking for valid response and data parsing
-
-//   const businesses = jsonObject['bussinessDirectory'];
-
-//   businesses.forEach(displaybusinesses);
-//   });
-
- 
-
-
-// function displaybusinesses(directoryBusinnes) {
-//     // date of birth , birth place and directoryBusinnes images
-//    const card = document.createElement('section');
-//    const image = document.createElement('img');
-//    const p1 = document.createElement('p');
-//    const p2 = document.createElement('a');
-//    const p3 = document.createElement('p');
-  
-
-//     p3.textContent = `${directoryBusinnes.companyAdress} `;
-//     card.appendChild(p3);
-    
-//     p1.textContent = ` ${directoryBusinnes.phonenumber}`;
-//     card.appendChild(p1);
-
-//     p2.textContent = ` ${directoryBusinnes.website}`;
-//     card.appendChild(p2);
-  
-//     image.src = directoryBusinnes.imageurl;
-//     image.setAttribute('alt', directoryBusinnes.order);
-//     card.appendChild(image);
-  
-  
-//     document.querySelector('div.bs').appendChild(card);
-  
-//   }
-
-const requestURL = 'https://anesushangwa.github.io/wdd230/Lesson9/datajson/another.json';
+const requestURL = 'https://piotrsherman.github.io/wdd-230/lesson9/another.json';
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
 
-  const busineses = jsonObject['busineses'];
+        const busineses = jsonObject['busineses'];
 
-  busineses.forEach(displaybusineses);
-  });
+        busineses.forEach(displaybusineses);
+    });
 
- 
+
 
 
 function displaybusineses(business) {
     // date of birth , birth place and prophet images
-   const card = document.createElement('section');
-   const image = document.createElement('img');
-   const p1 = document.createElement('p');
-   const p3 = document.createElement('p');
-//    const p2 = document.createElement('p');
-   const p4 = document.createElement('p');
+    const card = document.createElement('section');
+    const image = document.createElement('img');
+    const p1 = document.createElement('p');
+    const p3 = document.createElement('p');
+    //    const p2 = document.createElement('p');
+    const p4 = document.createElement('p');
 
 
 
-   image.src = business.imageurl;
-   image.setAttribute('alt', business.order);
-   card.appendChild(image);
- 
+    image.src = business.imageurl;
+    image.setAttribute('alt', business.order);
+    card.appendChild(image);
+
 
     p3.textContent = ` Phone number:  ${business.phonenumber}`;
     card.appendChild(p3);
 
     p4.textContent = ` Location: ${business.name}`;
     card.appendChild(p4);
-    
+
     p1.textContent = ` Web site: ${business.website}`;
     card.appendChild(p1);
 
 
-  
-   
-  
+
+
+
     document.querySelector('div.cards').appendChild(card);
-  
-  }
+
+}
