95957a23de4449534a30f4d09d7066bbf34e9c2d
piotr.s.herman@gmail.com <piotr.s.herman@gmail.com>
2022-03-12 17:00:03 +0300

    commit

---
+++ chamber/js/weather.js
@@ -0,0 +1,19 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=f4b9995aabd085a514a96289382703db";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.querySelector('#temp').textContent = jsObject.main.temp;
+    });
+
+
+
+const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+
+const desc = jsObject.weather[0].description;
+
+
+document.querySelector('#icon-src').textContent = iconsrc;
+document.querySelector('#weathericon').setAttribute('src', iconsrc);
+document.querySelector('#weathericon').setAttribute('alt', desc);
+document.querySelector('figcaption').textContent = desc;
