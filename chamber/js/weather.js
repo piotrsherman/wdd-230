b98f2acde2dca8f68f0ccdbaaf02011cb4895c86
piotr.s.herman@gmail.com <piotr.s.herman@gmail.com>
2022-03-19 20:09:52 +0300

    commit

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,18 +1,15 @@
-fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=f4b9995aabd085a514a96289382703db")
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=f4b9995aabd085a514a96289382703db')
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
         document.querySelector('#temp').textContent = jsObject.main.temp;
+        const iconsrc = 'https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png';
+
+        const desc = jsObject.weather[0].description;
+
+
+        document.querySelector('#icon-src').textContent = iconsrc;
+        document.querySelector('#weathericon').setAttribute('src', iconsrc);
+        document.querySelector('#weathericon').setAttribute('alt', desc);
+        document.querySelector('figcaption').textContent = desc;
     });
-
-
-
-const iconsrc = 'https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png';
-
-const desc = jsObject.weather[0].description;
-
-
-document.querySelector('#icon-src').textContent = iconsrc;
-document.querySelector('#weathericon').setAttribute('src', iconsrc);
-document.querySelector('#weathericon').setAttribute('alt', desc);
-document.querySelector('figcaption').textContent = desc;
