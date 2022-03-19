92d6b19a8acdbdda225857aaa3f3d3db6dc2049d
piotr.s.herman@gmail.com <piotr.s.herman@gmail.com>
2022-03-19 19:04:31 +0300

    commit

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -3,17 +3,25 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.querySelector('#temp').textContent = jsObject.main.temp;
+        document.querySelector('#temp').textContent = Math.round(jsObject.main.temp_max - 273.15);
+
+
+        const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        const desc = jsObject.weather[0].description;
+        // document.querySelector('#icon-src').textContent = iconsrc;
+        document.querySelector('#weathericon').setAttribute('src', iconsrc);
+        document.querySelector('#weathericon').setAttribute('alt', desc);
+        document.querySelector('#speed').textContent = jsObject.wind.speed;
+        document.querySelector('figcaption').textContent = desc;
+
+
+        const t = Math.round(jsObject.main.temp_max - 273.15);
+        const s = jsObject.wind.speed;
+
+        if (t <= 50 && s > 3) {
+            const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)
+            document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
+        } else {
+            document.querySelector("#windchill").innerHTML = "N/A"
+        }
     });
-
-
-
-const iconsrc = 'https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png';
-
-const desc = jsObject.weather[0].description;
-
-
-document.querySelector('#icon-src').textContent = iconsrc;
-document.querySelector('#weathericon').setAttribute('src', iconsrc);
-document.querySelector('#weathericon').setAttribute('alt', desc);
-document.querySelector('figcaption').textContent = desc;
